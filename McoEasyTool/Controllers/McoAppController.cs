c10f4cd5fdbf8b9cfc7f1eb36a29e05675ec6716
Emmagit <gaeemmanuel@yahoo.fr>
2015-04-22 19:36:30 +0200

    5

--- McoEasyTool/Controllers/McoAppController.cs
+++ McoEasyTool/Controllers/McoAppController.cs
@@ -1403,7 +1403,7 @@ namespace McoEasyTool.Controllers
                 MySheet.Name = "Check" + DateTime.Now.ToString("dd") +
                     DateTime.Now.ToString("MM") + DateTime.Now.ToString("yyyy");
                 MySheet.Activate();
-                
+                int a = 3;
                 AppReport report = db.AppReports.Create();
                 report.DateTime = DateTime.Now;
                 report.TotalChecked = 0;
